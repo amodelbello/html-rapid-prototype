b37eaa62283d9145a8f703f2a81f7bde99b957b0
Amo DelBello <amodelbello@gmail.com>
2018-03-01 12:14:02 -0700

    Moves require statements into conditionals in app.js

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const cli = require('./helpers/cli');
-const init_task = require('./tasks/init');
-const build_task = require('./tasks/build');
-const generate_task = require('./tasks/generate');
-
 
 if (cli.arguments.init) {
+  const init_task = require('./tasks/init');
   init_task.generateBaseScaffold();
 }
 
 if (cli.arguments.build) {
+  const build_task = require('./tasks/build');
   build_task.build();
 }
 
+if (cli.arguments.generate) {
+  const generate_task = require('./tasks/generate');
+  build_task.build();
+}
 
 console.log("This is the beginning...");
