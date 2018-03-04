c7211c3be501a500cfe89a33b6ef59d8134deb5c
Amo DelBello <amodelbello@gmail.com>
2018-03-03 22:24:14 -0700

    Adds global install configuration, now just run with 'rp ...'

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
 const cli = require('./helpers/cli');
 
-if (cli.arguments.init) {
-  const init_task = require('./tasks/init');
-  init_task.createBaseScaffold();
-}
+exports.run = () => {
+  if (cli.arguments.init) {
+    const init_task = require('./tasks/init');
+    init_task.createBaseScaffold();
+  }
 
-if (cli.arguments.build) {
-  const build_task = require('./tasks/build');
-  build_task.build();
-}
+  if (cli.arguments.build) {
+    const build_task = require('./tasks/build');
+    build_task.build();
+  }
 
-if (cli.arguments.generate) {
-  const generate_task = require('./tasks/generate');
-  const build_task = require('./tasks/build');
+  if (cli.arguments.generate) {
+    const generate_task = require('./tasks/generate');
+    const build_task = require('./tasks/build');
 
-  generate_task.generateNewContentFiles()
-  .then(() => build_task.build())
-  .catch(e => {
-    console.log(`Could not generate file(s): ${e}`);
-  });
-}
+    generate_task.generateNewContentFiles()
+    .then(() => build_task.build())
+    .catch(e => {
+      console.log(`Could not generate file(s): ${e}`);
+    });
+  }
 
-if (cli.arguments.watch) {
-  const watch_task = require('./tasks/watch');
-}
+  if (cli.arguments.watch) {
+    const watch_task = require('./tasks/watch');
+  }
+};
